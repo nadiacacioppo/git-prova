74f2ba6f4970b531dd829c345a84cdf3ca9dedc9
nadia <nadiacacioppo@gmail.com>
2023-11-06 15:54:03 +0100

    Inserito tag p

--- src/pages/pageCacioppo.tsx
+++ src/pages/pageCacioppo.tsx
@@ -2,7 +2,7 @@ const PageCacioppo = () => {
     return (
         <>
             <h3>Nadia Cacioppo</h3>
-            <p>Ho creato una nuova branch chiamata BranchCacioppo e ho fatto il merge di master, dopo di che ho fatto il reset di master riportandola alla commit precedente.</p>
+            <p>Basta fare un merge.</p>
         </>
     )
   };
