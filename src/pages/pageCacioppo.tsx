0d63abc01c97cb6928f508f809c34d861595ef9c
nadia <nadiacacioppo@gmail.com>
2023-11-06 16:01:29 +0100

    Aggiunto antipasto
    Aggiunto primo
    
    Aggiunto secondo
    
    Aggiunto dolce
    
    aggiunta risposta punto 5.9

--- src/pages/pageCacioppo.tsx
+++ src/pages/pageCacioppo.tsx
@@ -3,6 +3,11 @@ const PageCacioppo = () => {
         <>
             <h3>Nadia Cacioppo</h3>
             <p>Ho creato una nuova branch chiamata BranchCacioppo e ho fatto il merge di master, dopo di che ho fatto il reset di master riportandola alla commit precedente.</p>
+            <p>Tartine al salmone</p>
+            <p>Pasta al pesto</p>
+            <p>Parmigiana di melanzane</p>
+            <p>Tiramisù</p>
+            <p>Per unire molti commit, bisognare fare la rebase -i della branch e sostituire pick a squash dei commit che ci interessano.</p>
         </>
     )
   };
