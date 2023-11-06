9dcabd8cb0b19c4875927307a0d242cfe78a36d3
nadia <nadiacacioppo@gmail.com>
2023-11-06 17:14:54 +0100

    aggiunto file ammenda.tsx

---
+++ src/pages/Ammenda.tsx
@@ -0,0 +1,9 @@
+const Ammenda = () => {
+    return (
+        <>
+            <p>Per cambiare il messaggio di una commit bisognare fare il rebase -i e sostituire il pick della commit interessata con reword e modificare il messaggio.</p>
+        </>
+    )
+  };
+  
+  export default Ammenda;
